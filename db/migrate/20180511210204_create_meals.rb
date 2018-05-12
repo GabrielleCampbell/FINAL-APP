da2798eaba8a9876edb70d35a6e8b1ae7eddadbe
Gabrielle Campbell <gabi@Gabrielles-MacBook-Pro.local>
2018-05-11 22:32:06 -0400

    First Commit

---
+++ db/migrate/20180511210204_create_meals.rb
@@ -0,0 +1,14 @@
+class CreateMeals < ActiveRecord::Migration[5.1]
+  def change
+    create_table :meals do |t|
+      t.string :vegetable
+      t.string :protein
+      t.string :grain
+      t.string :fruit
+
+      t.timestamps
+    end
+  end
+end
+
+
